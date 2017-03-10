310ab014a11dd2ddc0121bd3347d96516f40c106
Ashish Tiwari <ashish@Ashishs-MacBook-Air.local>
2017-03-09 20:23:45 -0800

    Added code to quit browser

--- tablesread.py
+++ tablesread.py
@@ -8,12 +8,14 @@ driver.get("https://letskodeit.teachable.com/p/practice")
 driver.implicitly_wait(3)
 
 elements = driver.find_elements_by_xpath("//table[@id='product']//td")
+driver.implicitly_wait(3)
 
 print(len(elements))
 
 finalilist = [element.text for element in elements]
 
 print(finalilist)
+driver.quit()
 
 
 
